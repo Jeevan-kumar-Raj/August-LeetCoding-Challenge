8cfa2ffac38cb3efe874620edc540790348108c0
Jeevan Kumar <63276441+Jeevan-kumar-Raj@users.noreply.github.com>
2020-08-09 16:02:10 +0530

    Add files via upload

---
+++ Week-2/Day-2 Rotting Oranges.cpp
@@ -0,0 +1,84 @@
+Day-2 Rotting Oranges.cpp
+
+/*
+In a given grid, each cell can have one of three values:
+
+the value 0 representing an empty cell;
+the value 1 representing a fresh orange;
+the value 2 representing a rotten orange.
+Every minute, any fresh orange that is adjacent (4-directionally) to a rotten orange becomes rotten.
+
+Return the minimum number of minutes that must elapse until no cell has a fresh orange.  If this is impossible, return -1 instead.
+
+ 
+
+Example 1:
+
+
+
+Input: [[2,1,1],[1,1,0],[0,1,1]]
+Output: 4
+Example 2:
+
+Input: [[2,1,1],[0,1,1],[1,0,1]]
+Output: -1
+Explanation:  The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
+Example 3:
+
+Input: [[0,2]]
+Output: 0
+Explanation:  Since there are already no fresh oranges at minute 0, the answer is just 0.
+ 
+
+Note:
+
+1 <= grid.length <= 10
+1 <= grid[0].length <= 10
+grid[i][j] is only 0, 1, or 2.
+
+
+*/
+
+
+
+// Time:  O(m * n)
+// Space: O(m * n)
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        static const vector<pair<int, int>> directions{{0, 1}, {1, 0},
+                                                       {0, -1}, {-1, 0}};
+
+        int count = 0;
+        queue<tuple<int, int, int>> q;
+        for (int r = 0; r < grid.size(); ++r) {
+            for (int c = 0; c < grid[r].size(); ++c) {
+                if (grid[r][c] == 2) {
+                    q.emplace(r, c, 0);
+                } else if (grid[r][c]  == 1) {
+                    ++count;
+                }
+            }
+        }
+
+        int result = 0;
+        while (!q.empty()) {
+            int r, c;
+            tie(r, c, result) = q.front(); q.pop();
+            for (const auto& d : directions) {
+                int nr = r + d.first, nc = c + d.second;
+                if  (!(0 <= nr && nr < grid.size() &&
+                       0 <= nc && nc < grid[r].size())) {
+                    continue;
+                }
+                if (grid[nr][nc] == 1) {
+                    --count;
+                    grid[nr][nc] = 2;
+                    q.emplace(nr, nc, result + 1);
+                }
+            }
+        }
+        return (count == 0) ? result : -1;
+    }
+};
