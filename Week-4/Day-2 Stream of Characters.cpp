f89321675b83df0b4669991b01f717aac10284da
Jeevan Kumar <63276441+Jeevan-kumar-Raj@users.noreply.github.com>
2020-08-23 12:50:39 +0530

    Add files via upload

---
+++ Week-4/Day-2 Stream of Characters.cpp
@@ -0,0 +1,154 @@
+Day-2 Stream of Characters.cpp
+
+
+/*
+Implement the StreamChecker class as follows:
+
+StreamChecker(words): Constructor, init the data structure with the given words.
+query(letter): returns true if and only if for some k >= 1, the last k characters queried (in order from oldest to newest, including this letter just queried) spell one of the words in the given list.
+ 
+
+Example:
+
+StreamChecker streamChecker = new StreamChecker(["cd","f","kl"]); // init the dictionary.
+streamChecker.query('a');          // return false
+streamChecker.query('b');          // return false
+streamChecker.query('c');          // return false
+streamChecker.query('d');          // return true, because 'cd' is in the wordlist
+streamChecker.query('e');          // return false
+streamChecker.query('f');          // return true, because 'f' is in the wordlist
+streamChecker.query('g');          // return false
+streamChecker.query('h');          // return false
+streamChecker.query('i');          // return false
+streamChecker.query('j');          // return false
+streamChecker.query('k');          // return false
+streamChecker.query('l');          // return true, because 'kl' is in the wordlist
+ 
+
+Note:
+
+1 <= words.length <= 2000
+1 <= words[i].length <= 2000
+Words will only consist of lowercase English letters.
+Queries will only consist of lowercase English letters.
+The number of queries is at most 40000.
+
+
+
+*/
+
+
+
+
+struct AhoNode {
+    vector<AhoNode *> children;
+    vector<int> indices;
+    AhoNode *suffix;
+    AhoNode *output;
+    AhoNode() :
+        children(26, nullptr),
+        suffix(nullptr),
+        output(nullptr) {}
+};
+
+class AhoTrie {
+public:
+    AhoTrie(const vector<string>& patterns) : root_(createACTrie(patterns)) {
+        node_ = createACSuffixAndOutputLinks(root_);
+    }
+
+    vector<int> step(char letter) {
+        while (node_ && !node_->children[letter - 'a']) {
+            node_ = node_->suffix;
+        }
+        node_ = node_ ? node_->children[letter - 'a'] : root_;
+        return getACNodeOutputs(node_);
+    }
+
+private:
+    AhoNode *createACTrie(const vector<string>& patterns) {  // Time:  O(n), Space: O(t)
+        auto root = new AhoNode();
+        for (int i = 0; i < patterns.size(); ++i) {
+            auto node = root;
+            for (const auto& c : patterns[i]) {
+                if (!node->children[c - 'a']) {
+                    node->children[c - 'a'] = new AhoNode();
+                }
+                node = node->children[c - 'a'];
+            }
+            node->indices.emplace_back(i);
+        }
+        return root;
+    }
+    
+    AhoNode *createACSuffixAndOutputLinks(AhoNode *root) {  // Time:  O(n), Space: O(t)
+        queue<AhoNode *> q;
+        for (auto node : root->children) {
+            if (!node) {
+                continue;
+            }
+            q.emplace(node);
+            node->suffix = root;
+        }
+                
+        while (!q.empty()) {
+            auto node = q.front(); q.pop();
+            for (int c = 0; c < node->children.size(); ++c) {
+                if (!node->children[c]) {
+                    continue;
+                }
+                auto child = node->children[c];
+                q.emplace(child);
+                auto suffix = node->suffix;
+                while (suffix && !suffix->children[c]) {
+                    suffix = suffix->suffix;
+                }
+                child->suffix = suffix ? suffix->children[c] : root;
+                child->output = !child->suffix->indices.empty() ?
+                    child->suffix : child->suffix->output;
+            }
+        }
+        
+        return root;
+    }
+    
+    vector<int> getACNodeOutputs(AhoNode *node) {  // Time:  O(z), in this question, it could be improved to O(1)
+                                                   // if we only return a matched pattern without all matched ones
+        vector<int> result;
+        for (const auto& i : node_->indices) {
+            result.emplace_back(i);
+            // return result;
+        }
+        auto output = node_->output;
+        while (output) {
+            for (const auto& i : output->indices) {
+                result.emplace_back(i);
+                // return result;
+            }
+            output = output->output;
+        }
+        return result;
+    }
+    
+    AhoNode * const root_;
+    AhoNode *node_;
+};
+
+class StreamChecker {
+public:
+    StreamChecker(vector<string>& words) : trie_(words) {
+    }
+    
+    bool query(char letter) {  // O(m) times
+        return !trie_.step(letter).empty();
+    }
+
+private:
+    AhoTrie trie_;
+};
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker* obj = new StreamChecker(words);
+ * bool param_1 = obj->query(letter);
+ */
